bed9dcec119711bed98f0b3387ca3bc67769b88a
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-05-16 18:30:14 +0600

    calculator buttons bug fix

--- js/app.js
+++ js/app.js
@@ -16,12 +16,16 @@ document.getElementById('pin_btn').addEventListener('click', function(){
 document.getElementById('buttons').addEventListener('click', function(event){
     const num = event.target.innerText;
     const calDisplay = document.getElementById('cal_display');
-    const displayNum = calDisplay.value;
-    const updateNum = displayNum + num;
-    calDisplay.value = updateNum;
-    if(isNaN){
+    if(isNaN(num)){
         if(num == 'C'){
             calDisplay.value = '';
         }
+        
+    }
+    else {
+
+        const displayNum = calDisplay.value;
+        const updateNum = displayNum + num;
+        calDisplay.value = updateNum;
     }
 })
