86cf8bb6b4dc4df40c20a58367f77bf675248b9e
Syed Nur.A.Rabbi Jim <syednur.jim@gmail.com>
2023-05-16 16:42:07 +0600

    calculator part done

--- js/app.js
+++ js/app.js
@@ -12,3 +12,16 @@ document.getElementById('pin_btn').addEventListener('click', function(){
     const pinDisplay = document.getElementById('pin_display');
     pinDisplay.value = getPin();
 })
+
+document.getElementById('buttons').addEventListener('click', function(event){
+    const num = event.target.innerText;
+    const calDisplay = document.getElementById('cal_display');
+    const displayNum = calDisplay.value;
+    const updateNum = displayNum + num;
+    calDisplay.value = updateNum;
+    if(isNaN){
+        if(num == 'C'){
+            calDisplay.value = '';
+        }
+    }
+})
